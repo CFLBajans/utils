96581b87028cbca994c9c1fecb03179f8e3cc0a3
Kim Whitehall <k_whitehall@yahoo.com>
2017-07-10 21:41:16 -0400

    suite of scripts and files to automate sending reminder emails

---
+++ cronscripts/server-side/monthlyReminder.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script is for sending the mailing list the meeting reminders every second 
+# Saturday and the Thursday before the next meeting (third Satureday of the month)
+# Add to crontab like so: 0 0 8-14 * * [`date +"%a"`==Sun]&&/path/to/monthlyReminder.sh
+# Add to crontab like so: 0 0 * * * [`date +"%a"`==Thu]&&/path/to/monthlyReminder.sh
+
+# if Sun then run the python script, else check if the date is new date -3 days
+if [[ `date +"%a"` == "Sun" ]]; then
+	python /path/to/automate/sendemail.py
+else
+	source path/to/newDate.txt
+	nextSat=$(date -d "+3 days" +"%Y%m%d")
+	if [[ $nextSat == $saveDate ]]; then
+		python /path/to/automate/sendemail.py
+fi
